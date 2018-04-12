359c1d53d833a2ff063496fe0fba16edf747c836
Vahid Fazel-Rezai <vahidfazelrezai@gmail.com>
2018-04-12 07:26:28 -0400

    more revisions

--- assets/script.js
+++ assets/script.js
@@ -26,48 +26,70 @@ $(document).ready(function () {(function($) {
     offset: 54
   });
   
-      
-  $('#PEDD-tile').click(function () {$('#portfolio-title').text('PEDD')});
-  $('#CB-tile').click(function () {$('#portfolio-title').text('Cofee Beans')});
-  $('#AREZ-tile').click(function () {$('#portfolio-title').text('AREZ')});
-  $('#BBB-tile').click(function () {$('#portfolio-title').text('Beirute By Byte')});
-  $('#TA-tile').click(function () {$('#portfolio-title').text('The Alternative')});
-  $('#Kindi-tile').click(function () {$('#portfolio-title').text('Kindi')});
-  $('#DISCOVERY-tile').click(function () {$('#portfolio-title').text('DISCOVERY')});
-  $('#Maktabatech-tile').click(function () {$('#portfolio-title').text('Maktabatech')});
-  $('#BrainTank-tile').click(function () {$('#portfolio-title').text('BrainTank')});
-  $('#UL-tile').click(function () {$('#portfolio-title').text('Unique Learna')});
-  $('#Amal-tile').click(function () {$('#portfolio-title').text('Amal')});
-  $('#FS-tile').click(function () {$('#portfolio-title').text('Futuristic School')});
-  $('#Virtual-Teacher-tile').click(function () {$('#portfolio-title').text('Virtual-Teacher')});
-  $('#HIKMA-tile').click(function () {$('#portfolio-title').text('HIKMA')});
-  $('#Edutek-tile').click(function () {$('#portfolio-title').text('Edutek')});
-  $('#Pitchworthy-tile').click(function () {$('#portfolio-title').text('Pitchworthy')});
+  var portfolioContentURL = {
+    'Amal': 'assets/md/amal.md',
+    'AREZ': 'assets/md/arez.md',
+    'BBB': 'assets/md/beirut by byte.md',
+    'BrainTank': 'assets/md/braintank.md',
+    'CB': 'assets/md/coffee beans.md',
+    'DISCOVERY': 'assets/md/discovery.md',
+    'Edutek': 'assets/md/edutek.md',
+    'FS': 'assets/md/futuristic school.md',
+    'HIKMA': 'assets/md/hikma.md',
+    'Kindi': 'assets/md/kindi.md',
+    'Maktabatech': 'assets/md/maktabatech.md',
+    'PEDD': 'assets/md/pedd.md',
+    'Pitchworthy': 'assets/md/pitchworthy.md',
+    'TA': 'assets/md/the alternative.md',
+    'UL': 'assets/md/unique learna.md',
+    'VT': 'assets/md/virtual-teacher.md'
+  }
   
+  var portfolioTitle = {
+    'PEDD': 'PEDD',
+    'CB': 'Cofee Beans',
+    'AREZ': 'AREZ',
+    'BBB': 'Beirute By Byte',
+    'TA': 'The Alternative',
+    'Kindi': 'Kindi',
+    'DISCOVERY': 'DISCOVERY',
+    'Maktabatech': 'Maktabatech',
+    'BrainTank': 'BrainTank',
+    'UL': 'Unique Learna',
+    'Amal': 'Amal',
+    'FS': 'Futuristic School',
+    'VT': 'Virtual-Teacher',
+    'HIKMA': 'HIKMA',
+    'Edutek': 'Edutek',
+    'Pitchworthy': 'Pitchworthy'
+  }
   
-  var text = '## Problem\n\
-\n\
-When refugee students go to university, the immediately have a language problem -- much of the classes are in English. While teachers speak in Arabic, textbooks and exams are in English. Students must do a lot of translation work: they check for online help, make notes in Arabic, and translate just to prepare the material for studying.\n\
-\n\
-This is in addition to the usual problem of going to university and not knowing many people and not having as much social support.\n\
-\n\
-## Solution\n\
-\n\
-We empower refugee students at universities to help each other to make the learning experience easier and more efficient via a mobile application to overlay notes/hints/discussions/questions on textbooks via AR technology.\n\
-\n\
-Students usually have their phones with them when they study. Now they can pull out their phone and immediately get useful information. Students can add or see notes that have already been added, and ask questions about particular content. Questions are shared with other students on the app, and students can mark useful questions and answers and so students who are most helpful can be rewarded in the app. Teachers can use the app to check answers and emphasize important material.\n\
-\n\
-## Team Members\n\
-\n\
-Sabine Abou Ezzi <binael@hotmail.com>\n\
-\n\
-Youssef Chamoun <youssefchamoun@hotmail.com>\n\
-';
-  var md = window.markdownit();
+  function setPortfolioContent(teamId) {
+    console.log(teamId);
+    
+    jQuery.get(portfolioContentURL[teamId], function(data) {
+      $('#portfolio-title').text(portfolioTitle[teamId]);
+      var md = window.markdownit();
+      $('#portfolio-content').html(md.render(data));
+    });
+  }
   
-  console.log(md.render(text));
-
-  $('#portfolio-content').html(md.render(text));
+  $('#PEDD-tile').click(function () {setPortfolioContent('PEDD')});
+  $('#CB-tile').click(function () {setPortfolioContent('CB')});
+  $('#AREZ-tile').click(function () {setPortfolioContent('AREZ')});
+  $('#BBB-tile').click(function () {setPortfolioContent('BBB')});
+  $('#TA-tile').click(function () {setPortfolioContent('TA')});
+  $('#Kindi-tile').click(function () {setPortfolioContent('Kindi')});
+  $('#DISCOVERY-tile').click(function () {setPortfolioContent('DISCOVERY')});
+  $('#Maktabatech-tile').click(function () {setPortfolioContent('Maktabatech')});
+  $('#BrainTank-tile').click(function () {setPortfolioContent('BrainTank')});
+  $('#UL-tile').click(function () {setPortfolioContent('UL')});
+  $('#Amal-tile').click(function () {setPortfolioContent('Amal')});
+  $('#FS-tile').click(function () {setPortfolioContent('FS')});
+  $('#VT-tile').click(function () {setPortfolioContent('VT')});
+  $('#HIKMA-tile').click(function () {setPortfolioContent('HIKMA')});
+  $('#Edutek-tile').click(function () {setPortfolioContent('Edutek')});
+  $('#Pitchworthy-tile').click(function () {setPortfolioContent('Pitchworthy')});
   
 })(jQuery)});
 
